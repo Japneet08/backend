590a566232c17dc8374faf7cbce530a33184b5e4
Japneet-001 <japneetkb2204@gmail.com>
2024-12-06 20:25:03 +0530

    new changes added

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/twitter', (req,res)=>{
 
 app.get('/login',(req,res)=>{res.send('<h1>please login at chai aur code</h1>')})
 
+app.get('/start',(req,res)=>{
+  res.send("hello worlds yay jappa")
+})
+
 app.get('/youtube',(req,res)=>{res.send('<h2>chai aur code </h2>')})
 
 app.listen(process.env.PORT, () => {
